c2fc2d44b3185fe946350bae65e3d2d8a713426c
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2023-02-10 23:01:00 +0300

    update function added

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,151 @@
-import express from 'express';
-import mysql from 'mysql2';
+// import express from 'express';
+// import mysql from 'mysql2';
+// import cors from "cors";
+
+// const app = express();
+
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"ysfjon7222912",
+//     database:"test"
+// });
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/", (req,res)=> {
+//     res.json("Hello this is a backend");
+// });
+
+// app.get("/books", (req, res)=> {
+//     const q = "SELECT * FROM books";
+//     db.query(q,(err,data)=> {
+//         if(err) return res.json(err)
+//         return res.json(data)
+//     })
+// })
+
+// app.post("/books", (req,res)=> {
+//     const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
+//     const values = [
+//         req.body.title,
+//         req.body.desc,
+//         req.body.price,
+//         req.body.cover,
+//     ];
+
+//     db.query(q,[values], (err,data)=>{
+//         if(err) return res.json(err)
+//         return res.json("Book has been created")
+//     });
+// });
+
+// app.delete("/books/:id", (req,res)=> {
+//     const bookId = req.params.id;
+//     const q = "DELETE FROM books WHERE id = ?";
+
+//     db.query(q,[bookId], (err, data)=> {
+//         if(err) return res.json(err)
+//         return res.json("Book has been deleted")
+//     });
+// });
+
+// app.put("/books/:id", (req,res)=> {
+//     const bookId = req.params.id;
+//     const q = "UPDATE books SET `title` = ?, `desc`= ?, `price` = ? `cover`= ? WHERE id = ?";
+
+//     const values=[
+//         req.body.title,
+//         req.body.desc,
+//         req.body.price,
+//         req.body.cover,
+//     ]
+
+//     db.query(q, [ ...values, bookId ], (err, data)=> {
+//         if(err) return res.json(err)
+//         return res.json("Book has been updated")
+//     });
+// });
+
+// app.listen(8800, () => {
+//     console.log("Connected to backend!");
+// })
+
+
+import express from "express";
+import mysql from "mysql2";
 import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"ysfjon7222912",
-    database:"test"
+  host: "localhost",
+  user: "root",
+  password: "ysfjon7222912",
+  database: "test",
 });
 
-app.use(express.json());
-app.use(cors());
-
-app.get("/", (req,res)=> {
-    res.json("Hello this is a backend");
+app.get("/", (req, res) => {
+  res.json("hello");
 });
 
-app.get("/books", (req, res)=> {
-    const q = "SELECT * FROM books";
-    db.query(q,(err,data)=> {
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
-
-app.post("/books", (req,res)=> {
-    const q = "INSERT INTO books (`title`, `desc`, `price`, `cover`) VALUES (?)";
-    const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.price,
-        req.body.cover,
-    ];
-
-    db.query(q,[values], (err,data)=>{
-        if(err) return res.json(err)
-        return res.json("Book has been created")
-    });
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
 });
 
-app.delete("/books/:id", (req,res)=> {
-    const bookId = req.params.id;
-    const q = "DELETE FROM books WHERE id = ?";
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
 
-    db.query(q,[bookId], (err, data)=> {
-        if(err) return res.json(err)
-        return res.json("Book has been deleted")
-    })
-})
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
-    console.log("Connected to backend!");
-})
+  console.log("Connected to backend.");
+});
--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,11 +1,119 @@
-import React from 'react'
+// import axios from 'axios';
+// import React, { useState } from 'react'
+// import { useLocation, useNavigate } from 'react-router-dom';
+
+// const Update = () => {
+
+//     const [ book, setBook ] = useState({
+//         title: "",
+//         desc: "",
+//         price: null,
+//         cover: "",
+//     });
+
+//     const navigate = useNavigate();
+//     const location = useLocation();
+
+//     const bookId = location.pathname.split("/")[2];
+
+//     const handleChange = (e) => {
+//         setBook(prev=>({ ...prev, [e.target.name]: e.target.value }));
+//     };
+
+//     const handleClick = async e =>{
+//         e.preventDefault();
+//         try{
+//             await axios.put("http://localhost:8800/books/"+bookId, book)
+//             navigate("/");
+//         }catch(err){
+//             console.log(err)
+//         }
+//     }
+
+//   return (
+//     <div className='form'>
+//       <h1>Update the Book</h1>
+//       <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+//       <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
+//       <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+//       <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+//       <button className='formButton' onClick={handleClick}>Update</button>
+//     </div>
+//   )
+// }
+
+// export default Update
+
+
+
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Update = () => {
-  return (
-    <div>
-      Update
-    </div>
-  )
-}
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+  const [error,setError] = useState(false)
 
-export default Update
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const bookId = location.pathname.split("/")[2];
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.put(`http://localhost:8800/books/${bookId}`, book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="form">
+      <h1>Update the Book</h1>
+      <input
+        type="text"
+        placeholder="Book title"
+        name="title"
+        onChange={handleChange}
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Book desc"
+        name="desc"
+        onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Book price"
+        name="price"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>Update</button>
+      {error && "Something went wrong!"}
+      <Link to="/">See all books</Link>
+    </div>
+  );
+};
+
+export default Update;
+
